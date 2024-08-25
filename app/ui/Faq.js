dddf1439404f8ad9ac61a9d9cbd06821ce8e4d61
Pirai-Jawahar <jawahars@piraiinfo.com>
2024-08-25 16:20:47 +0530

    How it work page changes done

--- app/ui/Faq.js
+++ app/ui/Faq.js
@@ -47,7 +47,7 @@ const questions = [
 const defaultActiveKey = [0];
 const Faq = () => {
   return (
-    <div className="w-[90%] lg:w-[80%] m-auto rounded-[20px] sm:rounded-[80px] bg-grad-1 px-8 py-20 md:p-20 ">
+    <div className="w-[90%] lg:w-[70%] m-auto rounded-[20px] sm:rounded-[60px] bg-grad-1 px-1 py-20 md:p-20 ">
       <h3 className="text-center pb-10">Frequently Asked Questions</h3>
       <Collapse
         expandIcon={expandIcon}
