34c2bbc90c94c6a6cf6c4cb79be12c67fe6ec710
krishnan-jaga <krishnanj@piraiinfo.com>
2024-08-30 16:09:32 +0530

    review changes and responsive changes

--- app/Compliance/page.js
+++ app/Compliance/page.js
@@ -13,12 +13,12 @@ function page() {
     <div className="container m-auto px-6 sm:px-0">
       <section className="px-2 py-8 sm:px-4 md:px-8 md:py-12">
       <div className="text-center">
-        <span className="font-inter text-4xl md:text-6xl lg:text-7xl font-light leading-tight tracking-[-2px] text-center bg-clip-text text-transparent bg-gradient-to-r from-[#9281FF] via-[#3EA8BD] to-[#89D6FF]">
+        <span className="font-inter text-[50px] lg:text-[88px] font-light leading-[106.5px] lg:leading-[60.51px] tracking-[-2px] text-center bg-clip-text text-transparent bg-gradient-to-r from-[#9281FF] via-[#3EA8BD] to-[#89D6FF]">
             Compliance
           </span>
         </div>
         <div className="w-full flex justify-center">
-          <p className="w-full max-w-[370px] px-8 md:max-w-md lg:max-w-full text-center font-circular-spotify text-[18px] sm:text-[22px] md:text-[27px] font-light leading-normal sm:leading-[30px] md:leading-[38.1px] text-[#404040] mt-4">
+          <p className="w-full max-w-[370px] px-8 md:max-w-md lg:max-w-full text-center font-circular-spotify text-[23px] lg:text-[27px] font-light leading-[32.45px] lg:leading-[38.1px] text-[#404040] mt-4">
             Checkbook.io Audits and Compliance Reports
           </p>
         </div>
@@ -31,11 +31,11 @@ function page() {
                 <div className="w-auto lg:w-full flex justify-start md:justify-normal lg:justify-end">
                   <Image src={topRightarrow} alt="topRightarrow" />
                 </div>
-                <h4 className="text-lg pt-10 sm:pt-0 lg:text-xl lg:w-[70%] lg:pl-4">
+                <h4 className="pt-10 sm:pt-0 text-[22px] lg:text-[24px] leading-[30.77px] lg:leading-[25.4px] lg:w-[70%] lg:pl-4">
                   PCI DSS
                 </h4>
               </div>
-              <p className="py-2 mt-2 lg:pl-4 text-sm sm:text-base lg:text-lg">
+              <p className="py-2 mt-2 lg:pl-4 text-[16px] lg:text-[18px] leading-[22.58px] lg:leading-[25.4px]">
                 The Payment Card Industry Data Security Standard is an
                 information security standard for organizations that handle
                 branded credit cards from the major card schemes.
@@ -48,11 +48,11 @@ function page() {
                 <div className="w-auto lg:w-full flex justify-start md:justify-normal lg:justify-end">
                   <Image src={topRightarrow} alt="topRightarrow" />
                 </div>
-                <h4 className="text-lg pt-10 sm:pt-0 lg:text-xl lg:w-[70%] lg:pl-4">
+                <h4 className="pt-10 sm:pt-0 text-[22px] lg:text-[24px] leading-[30.77px] lg:leading-[25.4px] lg:w-[70%] lg:pl-4">
                   SOC 1
                 </h4>
               </div>
-              <p className="py-2 mt-2 lg:pl-4 text-sm sm:text-base lg:text-lg">
+              <p className="py-2 mt-2 lg:pl-4 text-[16px] lg:text-[18px] leading-[22.58px] lg:leading-[25.4px]">
                 A SOC 1 Report (System and Organization Controls Report) is a
                 report on Controls at a Service Organization which are relevant
                 to user entities&apos; internal control over financial reporting.
@@ -67,11 +67,11 @@ function page() {
                 <div className="w-auto lg:w-full flex justify-start md:justify-normal lg:justify-end">
                   <Image src={topRightarrow} alt="topRightarrow" />
                 </div>
-                <h4 className="text-lg pt-10 sm:pt-0 lg:text-xl lg:w-[70%] lg:pl-4">
+                <h4 className="pt-10 sm:pt-0 text-[22px] lg:text-[24px] leading-[30.77px] lg:leading-[25.4px] lg:w-[70%] lg:pl-4">
                   HIPAA
                 </h4>
               </div>
-              <p className="py-2 mt-2 lg:pl-4 text-sm sm:text-base lg:text-lg">
+              <p className="py-2 mt-2 lg:pl-4 text-[16px] lg:text-[18px] leading-[22.58px] lg:leading-[25.4px]">
                 The Health Insurance Portability and Accountability Act of 1996,
                 commonly known as HIPAA, is a series of regulatory standards
                 that outline the lawful use and disclosure of protected health
@@ -85,11 +85,11 @@ function page() {
                 <div className="w-auto lg:w-full flex justify-start md:justify-normal lg:justify-end">
                   <Image src={topRightarrow} alt="topRightarrow" />
                 </div>
-                <h4 className="text-lg pt-10 sm:pt-0 lg:text-xl lg:w-[70%] lg:pl-4">
+                <h4 className="pt-10 sm:pt-0 text-[22px] lg:text-[24px] leading-[30.77px] lg:leading-[25.4px] lg:w-[70%] lg:pl-4">
                   SOC 2
                 </h4>
               </div>
-              <p className="py-2 mt-2 lg:pl-4 text-sm sm:text-base lg:text-lg">
+              <p className="py-2 mt-2 lg:pl-4 text-[16px] lg:text-[18px] leading-[22.58px] lg:leading-[25.4px]">
                 SOC 2 is an auditing procedure that ensures your service
                 providers securely manage your data to protect the interests of
                 your organization and the privacy of its clients.
@@ -100,20 +100,20 @@ function page() {
       </section>
 
       <section className="mt-14 md:mt-20 lg:mt-28">
-        <div className="w-full grid justify-center px-4">
-          <p className="font-circular-spotify text-base sm:text-lg md:text-xl lg:text-[27px] font-light leading-normal sm:leading-[30px] md:leading-[36px] lg:leading-[38.1px] text-center m-auto max-w-full sm:max-w-lg md:max-w-[556px] lg:max-w-[936px] text-[#404040]">
+        <div className="w-full grid justify-center px-1">
+          <p className="font-circular-spotify text-[22px] md:px-[80px] lg:px-[0px] lg:text-[27px] font-light leading-[31.04px] lg:leading-[38.1px] text-center m-auto max-w-full lg:max-w-[986px] text-[#404040]">
             We take compliance seriously and display our compliance efforts
             publicly. We are happy to share more details. Please contact
             compliance at
             <Link
               href="#"
-              className="font-circular-spotify text-base sm:text-lg md:text-xl lg:text-[27px] font-light leading-normal sm:leading-[30px] md:leading-[36px] lg:leading-[38.1px] text-[#3D77EA] border-b-2 border-[#3D77EA]"
+              className="font-circular-spotify text-[22px] lg:text-[27px] font-light leading-[31.04px] lg:leading-[38.1px] text-[#3D77EA] border-b-2 border-[#3D77EA]"
             >
               {" "}
               checkbook.io
             </Link>
           </p>
-          <p className="mt-8 sm:mt-10 lg:mt-14 font-circular-spotify text-base sm:text-lg md:text-xl lg:text-[27px] font-light leading-normal sm:leading-[30px] md:leading-[36px] lg:leading-[38.1px] text-center m-auto max-w-full sm:max-w-lg md:max-w-[556px] lg:max-w-[936px] text-[#404040]">
+          <p className="mt-8 sm:mt-10 lg:mt-14 font-circular-spotify text-[22px] lg:text-[27px] font-light leading-[31.04px] lg:leading-[38.1px] text-center m-auto max-w-full lg:max-w-[936px] text-[#404040]">
             Checkbook Payment Holdings, Inc.; NMLS ID #2535348
           </p>
           <div className="mt-10 sm:mt-14 mb-10 sm:mb-14 justify-center hidden sm:flex">
--- app/ui/CTAButton.js
+++ app/ui/CTAButton.js
@@ -1,38 +1,3 @@
-import Image from "next/image";
-import React from "react";
-
-function CTAButton({
-  icon,
-  backgroundType,
-  backgroundColor,
-  textColor,
-  content,
-  onClick,
-  // paddingx,
-  // paddingy,
-  // textSize,
-  // rounderTL,
-  // rounderTR,
-  // rounderBR,
-  // rounderBL,
-}) {
-  return (
-    <>
-      <button
-        onClick={onClick}
-        className={`flex items-center px-9 py-4 rounded-tr-[60px] rounded-br-[60px] rounded-bl-[40px] 
-          rounded-tl-none text-[17px]
-             ${backgroundType} ${backgroundColor} ${textColor}`}
-      >
-        {content}
-        {icon && <Image src={icon} alt="Arrow" className="ml-4" />}
-      </button>
-    </>
-  );
-}
-
-export default CTAButton;
-
 // import Image from "next/image";
 // import React from "react";
 
@@ -42,20 +7,22 @@ export default CTAButton;
 //   backgroundColor,
 //   textColor,
 //   content,
-//   paddingx,
-//   paddingy,
-//   textSize,
-//   rounderTL,
-//   rounderTR,
-//   rounderBR,
-//   rounderBL,
+//   onClick,
+//   // paddingx,
+//   // paddingy,
+//   // textSize,
+//   // rounderTL,
+//   // rounderTR,
+//   // rounderBR,
+//   // rounderBL,
 // }) {
 //   return (
 //     <>
 //       <button
-//         className={`flex items-center
-//              ${backgroundType} ${backgroundColor} ${textColor} ${paddingx}
-//              ${paddingy} ${textSize} ${rounderTL} ${rounderTR} ${rounderBR} ${rounderBL}`}
+//         onClick={onClick}
+//         className={`flex items-center px-9 py-4 rounded-tr-[60px] rounded-br-[60px] rounded-bl-[40px] 
+//           rounded-tl-none text-[17px]
+//              ${backgroundType} ${backgroundColor} ${textColor}`}
 //       >
 //         {content}
 //         {icon && <Image src={icon} alt="Arrow" className="ml-4" />}
@@ -65,3 +32,36 @@ export default CTAButton;
 // }
 
 // export default CTAButton;
+
+import Image from "next/image";
+import React from "react";
+
+function CTAButton({
+  icon,
+  backgroundType,
+  backgroundColor,
+  textColor,
+  content,
+  paddingx,
+  paddingy,
+  textSize,
+  rounderTL,
+  rounderTR,
+  rounderBR,
+  rounderBL,
+}) {
+  return (
+    <>
+      <button
+        className={`flex items-center
+             ${backgroundType} ${backgroundColor} ${textColor} ${paddingx}
+             ${paddingy} ${textSize} ${rounderTL} ${rounderTR} ${rounderBR} ${rounderBL}`}
+      >
+        {content}
+        {icon && <Image src={icon} alt="Arrow" className="ml-4" />}
+      </button>
+    </>
+  );
+}
+
+export default CTAButton;
