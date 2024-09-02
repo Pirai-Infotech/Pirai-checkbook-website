283069af4205ff68e89cdb126a3d06c62d98a870
User-019 <sakthivel-ravi>
2024-09-02 19:32:55 +0530

    Header and Footer UI issue fixed

--- app/ui/Header.js
+++ app/ui/Header.js
@@ -14,7 +14,7 @@ function Header() {
 
   return (
     <>
-      <div className="container m-auto flex justify-between items-center py-[35px] px-[5%] md:px-0">
+      <div className="container m-auto flex justify-between items-center py-[35px] px-[5%] sm:px-0">
         <div className="lg:w-[17%] 2xl:w-[15%] md:w-[28%]">
           <Image src={logo} alt="Checkbook" />
         </div>
@@ -39,45 +39,67 @@ function Header() {
         </div>
         {mobileMenuFlag ? (
           <>
-            <div className="block lg:hidden fixed w-full bg-[#3D77EB] px-[5%] pt-[30px] pb-14 left-0 top-0 z-50">
-              <div className="container m-auto ">
+            <div className="block lg:hidden absolute w-full bg-[#3D77EB] px-[5%] pt-[40px] pb-14 left-0 top-0 z-50 shadow-4xl">
+              <div className="container m-auto">
                 <div className="flex items-center justify-between">
-                  <Image src={mobileLogo} alt="Checkbook" />
-                  <div
-                    className="bg-white rounded-[3px] w-[30px] h-[30px]"
-                    onClick={() => setMobileMenuFlag(false)}
-                  >
-                    <Image src={closeIcon} alt="Close" />
+                  <div className="lg:w-[17%] 2xl:w-[15%] md:w-[28%]">
+                    <Image
+                      src={mobileLogo}
+                      alt="Checkbook"
+                      className="w-full"
+                    />
+                  </div>
+                  <div className="w-[75%] flex justify-end">
+                    <div
+                      className="bg-white rounded-[3px] w-[30px] h-[30px]"
+                      onClick={() => setMobileMenuFlag(false)}
+                    >
+                      <Image src={closeIcon} alt="Close" />
+                    </div>
                   </div>
                 </div>
                 <div className="flex items-start justify-between mt-5 flex-wrap">
-                  <div className="sm:w-[47%] w-full">
+                  <div className="md:w-[50%] sm:w-full w-full">
                     <ul>
-                      <li className="text-white text-xl font-extrabold border-b border-white py-4 uppercase">
-                        Home
+                      <li className=" border-b border-white py-4">
+                        <p className="text-white text-xl font-extrabold uppercase">
+                          Home
+                        </p>
                       </li>
-                      <li className="text-white text-xl font-extrabold border-b border-white py-4 uppercase flex flex-wrap lg:bg-none bg-white bg-opacity-10 p-5 lg:p-0">
-                        Products
+                      <li className="border-b border-white py-4 flex flex-wrap lg:bg-none bg-white bg-opacity-10 p-5 lg:p-0">
+                        <p className="text-white text-xl font-extrabold uppercase">
+                          Products
+                        </p>
                         <PaymentSubMenu />
                       </li>
-                      <li className="text-white text-xl font-extrabold border-b border-white py-4 uppercase">
-                        Solutions
+                      <li className="border-b border-white py-4">
+                        <p className="text-white text-xl font-extrabold uppercase">
+                          Solutions
+                        </p>
                       </li>
-                      <li className="text-white text-xl font-extrabold border-b border-white py-4 uppercase">
-                        Resources
+                      <li className="border-b border-white py-4">
+                        <p className="text-white text-xl font-extrabold uppercase">
+                          Resources
+                        </p>
                       </li>
                     </ul>
                   </div>
-                  <div className="sm:w-[47%] w-full">
+                  <div className="md:w-[44%] sm:w-full w-full">
                     <ul>
-                      <li className="text-white text-xl font-extrabold border-b border-white py-4 uppercase">
-                        Industries
+                      <li className="border-b border-white py-4">
+                        <p className="text-white text-xl font-extrabold uppercase">
+                          Industries
+                        </p>
                       </li>
-                      <li className="text-white text-xl font-extrabold border-b border-white py-4 uppercase flex flex-wrap">
-                        ROI Calculator
+                      <li className="border-b border-white py-4 flex flex-wrap">
+                        <p className="text-white text-xl font-extrabold uppercase">
+                          ROI Calculator
+                        </p>
                       </li>
-                      <li className="text-white text-xl font-extrabold border-b border-white py-4 uppercase">
-                        Pricing
+                      <li className="border-b border-white py-4">
+                        <p className="text-white text-xl font-extrabold uppercase">
+                          Pricing
+                        </p>
                       </li>
                     </ul>
                     <div className="mt-6">
